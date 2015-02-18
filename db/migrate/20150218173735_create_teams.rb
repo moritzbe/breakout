fd248c8828ee646872d362ac5f63fb468ba0f60c
moritzbe <moritzberthold@hotmail.com>
2015-02-18 19:52:20 +0100

    fixed major sqlite bug. Now postgresql

---
+++ db/migrate/20150218173735_create_teams.rb
@@ -0,0 +1,18 @@
+class CreateTeams < ActiveRecord::Migration
+  def change
+    create_table :teams do |t|
+    	t.has_many :players
+    	t.has_many :sponsors
+    	t.has_many :comments
+    	t.has_many :challenges
+    	t.has_many :kilometers
+    	t.has_many :stops
+    	t.has_many :pictures
+    	t.string :teamname
+    	t.string :city
+        t.timestamps null: false
+    end
+  end
+end
+
+
---
+++ db/migrate/20150218180407_create_players.rb
@@ -0,0 +1,12 @@
+class CreatePlayers < ActiveRecord::Migration
+  def change
+    create_table :players do |t|
+	t.string :prename
+	t.string :surname
+	t.integer :age
+	t.string :email
+	t.integer :phone
+    t.timestamps null: false
+    end
+  end
+end
