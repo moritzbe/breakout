01eadf960298637c5dfbe4e8e9a15acc21aa3488
moritzbe <moritzberthold@hotmail.com>
2015-02-20 18:58:22 +0100

    changes background wrks

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
+  root 'welcome#home'
+
   get  "/register" => "registration#new"
   post "/register" => "registration#create"	
 	
   post   "/login" => "login#create"
   delete "/logout" => "login#destroy"
 
-  resources :teams, :except => [:create]
+  resources :start
+  # resources :teams, :except => [:create]
   resources :teams do
   resources :players
   end
