27faa3b8dc6f5ead3782987131382dbc1c283bbe
moritzbe <moritzberthold@hotmail.com>
2015-02-24 15:45:00 +0100

    Display on Map works

--- app/assets/javascripts/getsheet.js
+++ app/assets/javascripts/getsheet.js
@@ -2,91 +2,126 @@ $(document).ready(function(){
 "use strict";
 //xmlhttp requests not supported
 
+  var munich = [48.150487, 11.581243]
+  var url = "/welcome/map";
+
+
   var Liveblog = function () {
-    this.lat;
+    this.positions = [];
+    this.geolocation;
+    this.address;
     this.lon;
+    this.lat;
   };
 
 
+  Liveblog.prototype.onSuccess = function () {
+    this.lat = this.position.coords.latitude;
+    this.lon = this.position.coords.longitude;
+    document.getElementById("latitude").innerHTML = this.lat;
+    document.getElementById("longitude").innerHTML = this.lon;
+  };
 
-  var liveblogInstance = new Liveblog();
+  Liveblog.prototype.onSuccess2 = function () {
+    this.onSuccess();
+  };
 
-  function onSuccess(position){
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-    console.log(lat);
-    document.getElementById("latitude").innerHTML = lat;
-    document.getElementById("longitude").innerHTML = lon;
-  }
+  // Liveblog.prototype.addShowMapListener = function {
+  //   $("#showMap").on("click", function initialize() {
+  //       var mapOptions = {
+  //         center: new google.maps.LatLng(munich[0], munich[1]),
+  //         zoom: 10,
+  //         mapTypeId: google.maps.MapTypeId.ROADMAP
+  //       };
+  //       console.log(positionarray);
+  //       var map = new google.maps.Map(document.getElementById("map_canvas"),
+  //           mapOptions);
 
-  function onSuccess2(position){
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-    var url = "http://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lon+"&sensor=true_or_false";
-      $.ajax(url, {
-      success: function(data) {
-         console.log(data.results[0].formatted_address);
-         var address = data.results[0].formatted_address;
-         document.getElementById("address").innerHTML = address;
+  //       positionarray = getData();
 
-      },
-      error: function() {
-         console.log(error);
-      }
-    });
-  }
+  //       var marker, i;
+  //       for (i = 0; i < positionarray.size; i++) {  
+  //         marker = new google.maps.Marker({
+  //           position: new google.maps.LatLng(positionarray[i][0], positionarray[i][1]),
+  //           map: map
+  //         });
+  //       };
 
+  //   });
+  // };
 
-  function getData(){
-    console.log("start");
-    var url = "/welcome/map";
-    var result;
+  Liveblog.prototype.getPositions = function(){
+    var self = this;
     $.ajax({
       type: "GET",
       url: url,
       dataType: "json",
       success: function(data) {
           console.log(data);
-          var positionarray = [];
           data.forEach(function(entry){
-          var lat = entry.latitude;
-          var lon = entry.longitude;
-          positionarray.push([lat, lon]);
-          this.result = positionarray;
-        });
-          console.log(result)
+            var lat = entry.latitude;
+            var lon = entry.longitude;
+            self.positions.push([lat, lon]);
+          });
       },
       error: function() {
          console.log(error);
       }
     });
-  }
+  };
 
-  function onError(error){
-    alert("Geolocation error: " + error);
-  } 
-  
-  $("#getLocation").on("click", function() {
-    if (navigator.geolocation) {
-      console.log("Geolocation is supported!");
-      // Get permission and geolocation
-      navigator.geolocation.getCurrentPosition(onSuccess, onError);
-      var options = {enableHighAccuracy: true, maximumAge:30000, timeout: 60000};
-      navigator.geolocation.watchPosition(onSuccess, onError, options); 
-    };
-  });
+  // Liveblog.prototype.getGeolocation = function(callback) {
+  // $("#getLocation").on("click", function() {
+  //   if (navigator.geolocation) {
+  //     console.log("Geolocation is supported!");
+  //     // Get permission and geolocation
+  //     navigator.geolocation.getCurrentPosition(callback, onError);
+  //     var options = {enableHighAccuracy: true, maximumAge:30000, timeout: 60000};
+  //     navigator.geolocation.watchPosition(onSuccess, onError, options); 
+  //   };
+  // });
+  // }
 
-  $("#getAddress").on("click", function() {
-    if (navigator.geolocation) {
-      console.log("Geolocation is supported!");
-      // Get permission and geolocation
-      navigator.geolocation.getCurrentPosition(onSuccess2, onError);
-      var options = {enableHighAccuracy: true, maximumAge:30000, timeout: 60000};
-      navigator.geolocation.watchPosition(onSuccess, onError, options); 
-    };
-  });
+  // Liveblog.prototype.getAdress = function(callback) {
+  // $("#getAddress").on("click", function() {
+  //   if (navigator.geolocation) {
+  //     console.log("Geolocation is supported!");
+  //     // Get permission and geolocation
+  //     navigator.geolocation.getCurrentPosition(callback, onError);
+  //     var options = {enableHighAccuracy: true, maximumAge:30000, timeout: 60000};
+  //     navigator.geolocation.watchPosition(onSuccess, onError, options); 
+  //   };
+  // });
+  // } 
 
-  $("#getData").on("click", getData);
+  // function onSuccess2(position){
+  //   var lat = position.coords.latitude;
+  //   var lon = position.coords.longitude;
+  //   var url = "http://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lon+"&sensor=true_or_false";
+  //     $.ajax(url, {
+  //     success: function(data) {
+  //        console.log(data.results[0].formatted_address);
+  //        var address = data.results[0].formatted_address;
+  //        document.getElementById("address").innerHTML = address;
+
+  //     },
+  //     error: function() {
+  //        console.log(error);
+  //     }
+  //   });
+  // }
+
+
+  // function onError(error){
+  //   alert("Geolocation error: " + error);
+  // } 
+
+
+  var liveblogInstance = new Liveblog();
+  liveblogInstance.getPositions();
+
+
+  // $("#getData").on("click", getData);
 //-------------------------------------------------------------------
 //Display Map
 //Key=AIzaSyAq5jqy6DxgQBkk4KoTPgqEk2Pcwc0WfwE
@@ -96,16 +131,14 @@ $(document).ready(function(){
           zoom: 10,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        console.log(positionarray);
+
         var map = new google.maps.Map(document.getElementById("map_canvas"),
             mapOptions);
 
-        positionarray = getData();
-
         var marker, i;
-        for (i = 0; i < positionarray.size; i++) {  
+        for (i = 0; i < liveblogInstance.positions.length-1; i++) {  
           marker = new google.maps.Marker({
-            position: new google.maps.LatLng(positionarray[i][0], positionarray[i][1]),
+            position: new google.maps.LatLng(liveblogInstance.positions[i][0], liveblogInstance.positions[i][1]),
             map: map
           });
         };
