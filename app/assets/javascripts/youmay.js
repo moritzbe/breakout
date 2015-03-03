e2d48cd26a781172755dec49b3826950fac6841e
moritzbe <moritzberthold@hotmail.com>
2015-03-03 17:58:59 +0100

    editing Teams

--- app/assets/javascripts/youmay.js
+++ app/assets/javascripts/youmay.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
 
-  $(".baritems").append('<li id="sponsor-nav" role="presentation"><a href="#">Edit</a></li>')
-
-  $("#teams-nav").click(function(){
-    open('/register', '_self',false);
-  })
 
 
 
